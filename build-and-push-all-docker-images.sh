730c4c938b223154549d1479f77fbb0d9b5c9ba1
Christian Neverdal <cneve@equinor.com>
2019-05-16 20:33:14 +0200

    Add support for proxy settings in build-and-push-all-docker-images script

--- build-and-push-all-docker-images.sh
+++ build-and-push-all-docker-images.sh
@@ -8,6 +8,10 @@ ORGANIZATION_NAME=equinorta
 
 echo "Will build and push to ${ORGANIZATION_NAME}"
 
+NO_PROXY=$(env | grep -i no_proxy | head -n 1 | cut -d '=' -f 2)
+HTTP_PROXY=$(env | grep -i https_proxy | head -n 1 | cut -d '=' -f 2)
+HTTPS_PROXY=$(env | grep -i https_proxy | head -n 1 | cut -d '=' -f 2)
+
 cd images
 
 for IMAGE_NAME in `find . -maxdepth 1 -mindepth 1 -type d -printf '%f\n'`
@@ -15,7 +19,7 @@ do
     echo "Building image ${IMAGE_NAME}"
     cd "${IMAGE_NAME}"
     IMAGE_TAG="${ORGANIZATION_NAME}/${IMAGE_NAME}:latest"
-    docker build --pull --squash --tag ${IMAGE_TAG} .
+    docker build --pull --squash --tag ${IMAGE_TAG} --build-arg no_proxy="${NO_PROXY}" --build-arg http_proxy="${HTTP_PROXY}" --build-arg https_proxy="${HTTPS_PROXY}" $@ .
     echo "Pushing ${IMAGE_TAG}"
     docker push ${IMAGE_TAG} || echo Could not push
     cd -
